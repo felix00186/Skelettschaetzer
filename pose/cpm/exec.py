5c5004ea8a503ecedfcb8358ad91f425186b47c3
Felix Semt <felix.semt@web.de>
2025-06-13 22:28:24 +0200

    CPM: Bilder speichern und Daten auslesen an neue API angepasst

--- pose/cpm/exec.py
+++ pose/cpm/exec.py
@@ -1,6 +1,7 @@
 import os
 import json
 import cv2
+import mmcv
 from mmpose.apis import MMPoseInferencer
 
 # Lade die Namen der Gelenke
@@ -8,18 +9,17 @@ with open("./joint_names.json", "r") as f:
     joint_names = json.load(f)
 
 # Initialisiere den Inferencer
-dataset_path = os.environ["BASE_PATH"]
 pose_model_config = os.environ["POSE_CONFIG"]
-pose_model_weights = dataset_path + os.environ["POSE_CHECKPOINT"]
+pose_model_weights = os.environ["POSE_CHECKPOINT"]
 
-det_model_config = dataset_path + os.environ["DET_CONFIG"]
-det_model_weights = dataset_path + os.environ["DET_CHECKPOINT"]
+det_model_config = os.environ["DET_CONFIG"]
+det_model_weights = os.environ["DET_CHECKPOINT"]
 
 inferencer = MMPoseInferencer(
     pose2d=pose_model_config,
-    pose2d_weights=pose_model_weights,
-    det_model=det_model_config,
-    det_weights=det_model_weights
+    pose2d_weights=pose_model_weights#,
+    #det_model=det_model_config,
+    #det_weights=det_model_weights
 )
 
 # Verzeichnisse
@@ -29,6 +29,24 @@ os.makedirs(output_dir, exist_ok=True)
 
 # Alle MP4-Videos im Input-Ordner
 input_files = [f for f in os.listdir(input_dir) if f.endswith(".mp4")]
+
+import numpy
+def simplify(obj):
+    if isinstance(obj, numpy.ndarray):
+        return list(obj.shape)
+    elif isinstance(obj, dict):
+        return {key: simplify(value) for key, value in obj.items()}
+    elif isinstance(obj, list):
+        return [simplify(item) for item in obj]
+    elif isinstance(obj, tuple):
+        return tuple(simplify(item) for item in obj)
+    else:
+        try:
+            json.dumps(obj)
+        except:
+            return str(type(obj))
+        else:
+            return obj
 
 # Verarbeite jedes Video einzeln
 for file_name in input_files:
@@ -46,16 +64,18 @@ for file_name in input_files:
     result_generator = inferencer(input_path, return_vis=True)
 
     for result in result_generator:
-        vis_frame = result["visualization"]
+        vis_frame = result["visualization"][0]
         writer.write(vis_frame)
 
+        with open("/data/data.json", "w") as f:
+            json.dump(simplify(result), f, indent=2)
+
         frame_keypoints = []
-        instances = result["predictions"][0]["keypoints"]
+        instances = result["predictions"][0]
         for person in instances:
             person_dict = {
-                joint_names[i]: {"x": float(x), "y": float(y)}
-                for i, (x, y, score) in enumerate(person)
-                if score >= 0.1  # nur wenn erkennbar
+                joint_names[i]: {"x": float(point[0]), "y": float(point[1])}
+                for i, (point, score) in enumerate(zip(person["keypoints"], person["keypoint_scores"]))
             }
             frame_keypoints.append(person_dict)
 
