212b90cdc1c3dca1d822d33ce5693326d4b86d17
Felix Semt <felix.semt@web.de>
2025-06-26 12:47:01 +0200

    DensePose läuft wieder einwandfrei

--- pose/densepose/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p /data/densepose
-python /app/exec.py --input_dir /data/input --output_dir /data/densepose
--- pose/densepose/exec.py
+++ pose/densepose/exec.py
@@ -1,62 +1,77 @@
-import os
-import argparse
-import glob
+import subprocess
 import cv2
+import os
+import re
 
-from detectron2.config import get_cfg
-from detectron2.engine import DefaultPredictor
-from densepose import add_densepose_config
-from detectron2.utils.visualizer import Visualizer
-from detectron2.data import MetadataCatalog
 
-def setup_cfg():
-    cfg = get_cfg()
-    add_densepose_config(cfg)
-    cfg.merge_from_file("detectron2/projects/DensePose/configs/densepose_rcnn_R_50_FPN_WC1M_s1x.yaml")
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
-    cfg.MODEL.WEIGHTS = "https://dl.fbaipublicfiles.com/densepose/densepose_rcnn_R_50_FPN_WC1M_s1x/217144516/model_final_48a9d9.pkl"
-    cfg.MODEL.DEVICE = "cuda"
-    return cfg
+INPUT_DIR = "/data/input"
+OUTPUT_DIR = "/data/densepose"
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+os.makedirs("/app/cache", exist_ok=True)
+os.makedirs("/app/cache2", exist_ok=True)
 
-def process_video(video_path, output_path, predictor):
-    cap = cv2.VideoCapture(video_path)
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+files = os.listdir(INPUT_DIR)
+mp4_files = list(filter(lambda f: f.endswith(".mp4"), files))
+
+for file_name in mp4_files:
+    # Caches leeren
+    for cache in ["cache", "cache2"]:
+        for file in os.listdir(os.path.join("/app", cache)):
+            os.remove(os.path.join("/app", cache, file))
+
+    # Video laden und in Einzelbilder unterteilen
+    cap = cv2.VideoCapture(os.path.join(INPUT_DIR, file_name))
     fps = cap.get(cv2.CAP_PROP_FPS)
-
-    fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-    out = cv2.VideoWriter(output_path, fourcc, fps, (width, height))
-
-    metadata = MetadataCatalog.get("detectron2_dataset_train")
-
-    while cap.isOpened():
+    frame_count = 0
+    while True:
         ret, frame = cap.read()
         if not ret:
             break
-
-        outputs = predictor(frame)
-        v = Visualizer(frame[:, :, ::-1], metadata=metadata)
-        result = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-
-        out.write(result.get_image()[:, :, ::-1])
-
+        frame_filename = os.path.join("./cache", f"{frame_count}.jpg")
+        cv2.imwrite(frame_filename, frame)
+        frame_count += 1
     cap.release()
-    out.release()
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--input_dir", required=True)
-    parser.add_argument("--output_dir", required=True)
-    args = parser.parse_args()
+    # Ausführen der Skelettschaetzung auf den Bildern
+    env = os.environ.copy()
+    env["MKL_SERVICE_FORCE_INTEL"] = "1"
+    args = [
+        "python",
+        "detectron2/projects/DensePose/apply_net.py",
+        "show",
+        "detectron2/projects/DensePose/configs/densepose_rcnn_R_50_FPN_DL_s1x.yaml",      # config
+        "/app/model_final_0ed407.pkl",                   # model
+        "cache/*.jpg",                              # input
+        "dp_contour,bbox",                          # visualization
+        "--output", "cache2/densepose.png",         # output
+        "-v"
+    ]
+    result = subprocess.run(args, capture_output=True, text=True, env=env)
+    result = result.stdout + result.stderr
+    result = result.splitlines()
+    result = [s.split("apply_net]: ")[1] for s in result if re.search("^\[.* apply_net\]: ", s)]
+    result = list(filter(lambda s: s.startswith("Processing") or s .startswith("Output saved to"), result))
+    images = {}
+    for i in range(0, len(result), 2):
+        proc_line = result[i]
+        try:
+            save_line = result[i + 1]
+        except IndexError:
+            continue
+        input_img = re.search("^Processing (.*)$", proc_line).group(1)
+        output_img = re.search("^Output saved to (.*)$", save_line).group(1)
+        images[input_img] = output_img
 
-    cfg = setup_cfg()
-    predictor = DefaultPredictor(cfg)
-
-    os.makedirs(args.output_dir, exist_ok=True)
-    video_files = glob.glob(os.path.join(args.input_dir, "*.mp4"))
-
-    for video_path in video_files:
-        filename = os.path.basename(video_path)
-        output_path = os.path.join(args.output_dir, filename)
-        print(f"Processing {filename}...")
-        process_video(video_path, output_path, predictor)
+    # Bilder zu Video zusammensetzen
+    inputs = list(images.keys())
+    inputs.sort(key=lambda s: int(s.split("/")[1].split(".")[0]))
+    image_paths = [images[input_img] for input_img in inputs]
+    first_frame = cv2.imread(image_paths[0])
+    height, width, _ = first_frame.shape
+    size = (width, height)
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # MP4 Codec
+    video_writer = cv2.VideoWriter(os.path.join(OUTPUT_DIR, file_name), fourcc, fps, size)
+    for idx, img_path in enumerate(image_paths):
+        frame = cv2.imread(img_path)
+        video_writer.write(frame)
+    video_writer.release()
